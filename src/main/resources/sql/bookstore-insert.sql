511aae7a26aa883c805801fa90a32d1716c73136
Bill Zude <bzude54@gmail.com>
2015-06-04 23:50:43 -0700

    adding sql directory and sql table files

---
+++ src/main/resources/sql/bookstore-insert.sql
@@ -0,0 +1,153 @@
+INSERT INTO GENRE ( NAME )
+			values ('drama');
+
+INSERT INTO GENRE ( NAME )
+			values ('scifi');
+
+INSERT INTO GENRE ( NAME )
+			values ('biography');
+
+INSERT INTO GENRE ( NAME )
+			values ('non-fiction');
+
+INSERT INTO GENRE ( NAME )
+			values ('textbook');
+
+
+INSERT INTO BOOK (ISBN,   TITLE,   GENRE_ID, PRICE, DESCRIPTION )
+values('161729120X', 'Spring In Action' , 1, 29.99, 'Spring in Action, Fourth Edition is a hands-on guide to the Spring Framework, updated for version 4. ');
+
+INSERT INTO BOOK (ISBN,   TITLE,     GENRE_ID, PRICE, DESCRIPTION )
+values('193239415X', 'Hibernate In Action' , 1, 9.99, 'Hibernate in Action carefully explains the concepts you need, then gets you going.');
+
+INSERT INTO BOOK (ISBN,   TITLE,     GENRE_ID, PRICE, DESCRIPTION )
+values('1931520720', 'Stories of Your Life and Others' , 2, 12.25, ' includes his first eight published stories plus the author''s story notes ');
+
+INSERT INTO BOOK (ISBN,   TITLE,     GENRE_ID, PRICE, DESCRIPTION )
+values('1430241071', 'Pro Spring 3' , 1, 19.99, 'Pro Spring 3 updates the bestselling Pro Spring with the latest that the Spring Framework has to offer');
+
+INSERT INTO BOOK (ISBN,   TITLE,    GENRE_ID, PRICE, DESCRIPTION )
+values('978-0-7645-7483-2', 'Professional Java Development with the Spring Framework' , 1, 39.99, 'You will be guided through all the Spring features and see how they form a coherent whole');
+
+INSERT INTO BOOK (ISBN,   TITLE,     GENRE_ID, PRICE, DESCRIPTION )
+values('978-0-7645-4385-2', 'Expert One-on-One J2EE Design and Development' , 1, 59.99, 'It will help you to solve common problems with J2EE and avoid the expensive mistakes often made in J2EE projects');
+
+
+INSERT INTO BOOK (ISBN,   TITLE,     GENRE_ID, PRICE, DESCRIPTION )
+             values ('978-0553380163',   'A Brief History of Time',  4, 12.00,  'A landmark volume in science writing by one of the great minds of our time, Stephen Hawking’s book explores such profound questions as: How did the universe begin-and what made its start possible?');
+
+INSERT INTO BOOK (ISBN,   TITLE,     GENRE_ID, PRICE, DESCRIPTION )
+             values ('978-0810998025',   'Industrial Light and Magic - The Art of Innovation',    4,  57.50, 'An extensively illustrated oral history of the multi-Academy-Award winning visual effects company founded by George Lucas' );
+
+
+INSERT INTO BOOK (ISBN,   TITLE,     GENRE_ID, PRICE, DESCRIPTION )
+             values ('978-0441569595',   'Neuromancer',  3, 8.99, 'Case had been the sharpest data-thief in the business, until vengeful former employees crippled his nervous system. But now a new and very mysterious employer recruits him for a last-chance run.');
+
+
+INSERT INTO BOOK (ISBN,   TITLE,     GENRE_ID, PRICE, DESCRIPTION )
+             values ('978-0553293357',   'Foundation',   2, 14.50, 'For twelve thousand years the Galactic Empire has ruled supreme. Now it is dying. But only Hari Sheldon, creator of the revolutionary science of psychohistory, can see into the future--to a dark age of ignorance, barbarism, and warfare that will last thirty thousand years.');
+
+INSERT INTO BOOK (ISBN,   TITLE,     GENRE_ID, PRICE, DESCRIPTION )
+             values ('978-0555555555',   'I am a software developer',  3, 120000.00, 'For three years, I have been trying to get out of that hell hole COntent. Now it is going to happen.');
+
+
+
+
+INSERT INTO BOOK (ISBN,   TITLE,     GENRE_ID, PRICE, DESCRIPTION )
+             values ('978-987654321',   'TestBook1',  5, 10.00, 'test description 1');
+
+INSERT INTO BOOK (ISBN,   TITLE,     GENRE_ID, PRICE, DESCRIPTION )
+             values ('978-123456789',   'TestBook2',   2, 12.00, 'test description 2');
+
+INSERT INTO BOOK (ISBN,   TITLE,     GENRE_ID, PRICE, DESCRIPTION )
+             values ('978-222222222',   'TestBook3',   3, 14.00, 'test description 3');
+
+INSERT INTO BOOK (ISBN,   TITLE,     GENRE_ID, PRICE, DESCRIPTION )
+             values ('978-333333333',   'TestBook4',  4, 22.00, 'test description 4');
+
+INSERT INTO BOOK (ISBN,   TITLE,     GENRE_ID, PRICE, DESCRIPTION )
+             values ('978-949494949',   'TestBook5',  5, 55.00, 'test description 5');
+
+INSERT INTO BOOK (ISBN,   TITLE,     GENRE_ID, PRICE, DESCRIPTION )
+             values ('978-888888888',   'TestBook6',   4, 145.00, 'test description 6' );
+
+INSERT INTO BOOK (ISBN,   TITLE,     GENRE_ID, PRICE, DESCRIPTION )
+             values ('978-999999999',   'TestBook7',  3, 23.00, 'test description 7');
+
+INSERT INTO BOOK (ISBN,   TITLE,     GENRE_ID, PRICE, DESCRIPTION )
+             values ('978-232323232',   'TestBook8',  5, 77.00, 'test description 8');
+
+INSERT INTO BOOK (ISBN,   TITLE,     GENRE_ID, PRICE, DESCRIPTION )
+             values ('978-2525525255',   'TestBook9',  2, 39.00, 'test description 9');
+
+INSERT INTO BOOK (ISBN,   TITLE,     GENRE_ID, PRICE, DESCRIPTION )
+             values ('978-2345456345',   'TestBook10',  4,  110.00, 'test description 10');
+
+
+
+
+
+INSERT INTO AUTHOR (FIRST_NAME, LAST_NAME) values ('Craig','Walls');
+INSERT INTO AUTHOR (FIRST_NAME, LAST_NAME) values ('Christian','Bauer');
+INSERT INTO AUTHOR (FIRST_NAME, LAST_NAME) values ('Ted','Chiang');
+
+INSERT INTO AUTHOR (FIRST_NAME, LAST_NAME) values ('Clarence','Ho');
+INSERT INTO AUTHOR (FIRST_NAME, LAST_NAME) values ('Rob','Harrop');
+
+INSERT INTO AUTHOR (FIRST_NAME, LAST_NAME) values ('Rod','Johnson');
+INSERT INTO AUTHOR (FIRST_NAME, LAST_NAME) values ('Jurgen','Höller');
+INSERT INTO AUTHOR (FIRST_NAME, LAST_NAME) values ('Alef','Arendsen');
+INSERT INTO AUTHOR (FIRST_NAME, LAST_NAME) values ('Thomas','Risberg');
+INSERT INTO AUTHOR (FIRST_NAME, LAST_NAME) values ('Colin','Sampaleanu');
+
+INSERT INTO AUTHOR (FIRST_NAME, LAST_NAME) values ('Isaac', 'Asimov');
+INSERT INTO AUTHOR (FIRST_NAME, LAST_NAME) values ('William', 'Gibson');
+INSERT INTO AUTHOR (FIRST_NAME, LAST_NAME) values ('Pamela', 'Glintenkamp');
+INSERT INTO AUTHOR (FIRST_NAME, LAST_NAME) values ('Stephen', 'Hawking');
+
+INSERT INTO AUTHOR (FIRST_NAME, LAST_NAME) values ('Iman', 'Author1');
+INSERT INTO AUTHOR (FIRST_NAME, LAST_NAME) values ('Iman', 'Author2');
+INSERT INTO AUTHOR (FIRST_NAME, LAST_NAME) values ('Iman', 'Author3');
+INSERT INTO AUTHOR (FIRST_NAME, LAST_NAME) values ('Iman', 'Author');
+INSERT INTO AUTHOR (FIRST_NAME, LAST_NAME) values ('Shesan', 'Author');
+INSERT INTO AUTHOR (FIRST_NAME, LAST_NAME) values ('Mesan', 'Author');
+
+
+INSERT INTO BOOK_AUTHOR ( BOOK_ID ,AUTHOR_ID ) values (1,1);
+INSERT INTO BOOK_AUTHOR ( BOOK_ID ,AUTHOR_ID ) values (2,2);
+INSERT INTO BOOK_AUTHOR ( BOOK_ID ,AUTHOR_ID ) values (3,3);
+
+INSERT INTO BOOK_AUTHOR ( BOOK_ID ,AUTHOR_ID ) values (4,4);
+INSERT INTO BOOK_AUTHOR ( BOOK_ID ,AUTHOR_ID ) values (4,5);
+
+INSERT INTO BOOK_AUTHOR ( BOOK_ID ,AUTHOR_ID ) values (5,6);
+INSERT INTO BOOK_AUTHOR ( BOOK_ID ,AUTHOR_ID ) values (5,7);
+INSERT INTO BOOK_AUTHOR ( BOOK_ID ,AUTHOR_ID ) values (5,8);
+INSERT INTO BOOK_AUTHOR ( BOOK_ID ,AUTHOR_ID ) values (5,9);
+INSERT INTO BOOK_AUTHOR ( BOOK_ID ,AUTHOR_ID ) values (5,10);
+
+INSERT INTO BOOK_AUTHOR ( BOOK_ID ,AUTHOR_ID ) values (6,20);
+
+INSERT INTO BOOK_AUTHOR ( BOOK_ID ,AUTHOR_ID ) values (6,19);
+INSERT INTO BOOK_AUTHOR ( BOOK_ID ,AUTHOR_ID ) values (6,18);
+INSERT INTO BOOK_AUTHOR ( BOOK_ID ,AUTHOR_ID ) values (7,17);
+INSERT INTO BOOK_AUTHOR ( BOOK_ID ,AUTHOR_ID ) values (8,16);
+INSERT INTO BOOK_AUTHOR ( BOOK_ID ,AUTHOR_ID ) values (8,15);
+INSERT INTO BOOK_AUTHOR ( BOOK_ID ,AUTHOR_ID ) values (8,14);
+INSERT INTO BOOK_AUTHOR ( BOOK_ID ,AUTHOR_ID ) values (9,12);
+INSERT INTO BOOK_AUTHOR ( BOOK_ID ,AUTHOR_ID ) values (10,13);
+INSERT INTO BOOK_AUTHOR ( BOOK_ID ,AUTHOR_ID ) values (10,11);
+INSERT INTO BOOK_AUTHOR ( BOOK_ID ,AUTHOR_ID ) values (11,10);
+INSERT INTO BOOK_AUTHOR ( BOOK_ID ,AUTHOR_ID ) values (12,4);
+
+INSERT INTO BOOK_AUTHOR ( BOOK_ID ,AUTHOR_ID ) values (13,8);
+INSERT INTO BOOK_AUTHOR ( BOOK_ID ,AUTHOR_ID ) values (14,14);
+INSERT INTO BOOK_AUTHOR ( BOOK_ID ,AUTHOR_ID ) values (15,15);
+INSERT INTO BOOK_AUTHOR ( BOOK_ID ,AUTHOR_ID ) values (15,4);
+INSERT INTO BOOK_AUTHOR ( BOOK_ID ,AUTHOR_ID ) values (16,17);
+INSERT INTO BOOK_AUTHOR ( BOOK_ID ,AUTHOR_ID ) values (17,18);
+INSERT INTO BOOK_AUTHOR ( BOOK_ID ,AUTHOR_ID ) values (18,19);
+INSERT INTO BOOK_AUTHOR ( BOOK_ID ,AUTHOR_ID ) values (19,4);
+INSERT INTO BOOK_AUTHOR ( BOOK_ID ,AUTHOR_ID ) values (20,12);
+INSERT INTO BOOK_AUTHOR ( BOOK_ID ,AUTHOR_ID ) values (21,13);
+INSERT INTO BOOK_AUTHOR ( BOOK_ID ,AUTHOR_ID ) values (21,14);
